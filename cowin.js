f2cfe7812b6feb6470a4e64568a6e064fa041f1c
nishant <nishantkodambal@gmail.com>
2021-05-20 13:40:28 +0530

    front update and js file also included

--- cowin.js
+++ cowin.js
@@ -17,10 +17,10 @@ function availablity() {
     cost != '' &&
     available != ''
   ) {
-    console.log(pincode);
-    console.log(age);
-    console.log(cost);
-    console.log(available);
+    // console.log(pincode);
+    // console.log(age);
+    // console.log(cost);
+    // console.log(available);
     let i;
     for (i = 1; i <= date; i++) {
       var l, array_result;
@@ -34,20 +34,24 @@ function availablity() {
           `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${Original_date}`
         );
         l = await response.json();
-        console.log(l['centers']);
+        // console.log(l['centers']);
         if (l) {
-          console.log(l['centers']);
+          // console.log(l['centers']);
           array_result = [...l['centers']];
           array_result.forEach(element => {
-            console.log(element);
+            // console.log(element);
             element['sessions'].forEach(ex => {
-              console.log(ex['min_age_limit']);
-              if (ex['min_age_limit'] <= age && ex['available_capacity'] > 0) {
+              if (
+                ex['min_age_limit'] >= age &&
+                Number(ex['available_capacity']) > 0
+              ) {
+                console.log(typeof ex['min_age_limit']);
+                console.log(typeof age, 'age');
                 const row = document.createElement('TR');
                 const table = document.getElementById('table');
                 const th = row.insertCell(-1);
                 table.appendChild(row);
-                console.log(ex['min_age_limit']);
+                // console.log(ex['min_age_limit']);
                 remove_child();
                 // const row = document.createElement('TR');
                 const th1 = row.insertCell(0);
@@ -74,12 +78,12 @@ function availablity() {
                 th2.innerText = ex['available_capacity'];
                 th3.innerText = ex['vaccine'];
                 th4.innerText = ex['min_age_limit'];
-                th4.innerText = pincode;
-                th5.innerText = element['name'];
-                th6.innerText = element['state_name'];
-                th7.innerText = element['district_name'];
-                th8.innerText = element['address'];
-                th9.innerText = element['fee_type'];
+                th5.innerText = pincode;
+                th6.innerText = element['name'];
+                th7.innerText = element['state_name'];
+                th8.innerText = element['district_name'];
+                th9.innerText = element['block_name'];
+                th10.innerText = element['fee_type'];
                 table.appendChild(row);
                 clear_fields();
               }
